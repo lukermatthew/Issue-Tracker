c5a58e00141e29ed1e7fe2366519646631bd9e89
Mat <tan.matmercado@gmail.com>
2023-11-28 23:56:54 +0800

    feature:/NavBar style the link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
+import classNames from 'classnames';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiOutlineIssuesClose } from "react-icons/ai";
 
 const NavBar = () => {
+
+  const currentPath = usePathname();
 
   const links = [
     { label: "Dashboard", href: "/"},
@@ -13,9 +18,13 @@ const NavBar = () => {
         <Link href="/"><AiOutlineIssuesClose /></Link>
         <ul className='flex space-x-6'>
           {links.map(link => (
-            <li>
-              <Link className="text-zinc-500 hover:text-zinc-800" key={link.href} href={link.href}>{link.label}</Link>
-            </li>
+              <Link
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true
+              })}
+              key={link.href} href={link.href}>{link.label}</Link>
           ))}
            
            
