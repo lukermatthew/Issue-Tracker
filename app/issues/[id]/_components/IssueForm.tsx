861d58493bc5f04940e808b22ee7ba01717aa36d
Mat <tan.matmercado@gmail.com>
2023-12-01 22:52:05 +0800

    feat:/Update issues

--- app/issues/[id]/_components/IssueForm.tsx
+++ app/issues/[id]/_components/IssueForm.tsx
@@ -35,7 +35,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setIsSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) await axios.patch("/api/issues/" + issue.id, data);
+      else await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
       setIsSubmitting(false);
@@ -71,7 +72,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
           <ErrorMessage>{errors.description?.message}</ErrorMessage>
         )}
         <Button disabled={isSubmitting}>
-          Submit New Issues {isSubmitting && <Spinner />}
+          {issue ? "Update Issue" : "Submit New Issue"}{" "}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
