471726aa365df007aa278eb6d29362346fe3f147
Mat <tan.matmercado@gmail.com>
2023-12-09 09:07:08 +0800

    feat:/Add assigned issue to prisma

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -26,7 +26,7 @@ const AssigneeSelect = () => {
     isLoading,
   } = useQuery<User[]>({
     queryKey: ["users"],
-    queryFn: () => axios.get("/xapi/users").then((res) => res.data),
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
     staleTime: 60 * 1000,
     retry: 3,
   });
